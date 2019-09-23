b12628e610bbdb416efbb73744d2efaa40bf4ad9
Jose Luis <joseluisrojasaranda@gmail.com>
2019-09-22 20:18:47 -0500

    Preparando cambios para nachito

--- test_f360.py
+++ test_f360.py
@@ -8,8 +8,8 @@ import numpy as np
 
 def main():
     #path_to_model = "trained_models/f360_vgg_01/"
-    path_to_model = "trained_models/f360_MobileNetV2_06/"
-    path_to_imgs = "datasets/test-f360-google/"
+    path_to_model = "trained_models/f360_MobileNetV2_04/"
+    path_to_imgs = "datasets/test-f360-google2/"
 
     with open(path_to_model+"setup.json", "r") as data:
         setup = json.load(data)
@@ -18,9 +18,11 @@ def main():
     w, h, _ = setup["input_shape"]
     classes = setup["classes"]
 
+    print("[INFO] Loading model...")
     model = tf.keras.models.load_model(path_to_model+"model.h5")
+    _ = input("[INFO] Click to continue")
     #model = tf.keras.models.load_model(path_to_model+"checkpoints/model_checkpoint_5.h5")
-    img_paths = glob.glob(path_to_imgs + "*.jpg")
+    img_paths = glob.glob(path_to_imgs + "*")
     
     for img_path in img_paths:
         image = cv2.imread(img_path)
