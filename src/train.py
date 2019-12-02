f6804955c5d0722bfd9224545a4e44cd008e5ff7
Jose Luis <joseluisrojasaranda@gmail.com>
2019-12-01 19:41:04 -0600

    Multi-input model training variations

--- src/train.py
+++ src/train.py
@@ -32,23 +32,35 @@ def main():
 def train_setup():
     setup = {
         "info": """
-            Training MobileNetV2 with ImageNet weights and Adam optimizer.
+            Training MobileNetV2 with ImageNet weights and RMSprop optimizer.
             first training the last fully connected layer, then using
-            fine tunning with the last 100 layers
+            fine tunning from the 100th layer. 
             """,
-        "path": "trained_models_FINAL/AOB_MobileNetV2_finetune_01/",
-        "dataset_path": "datasets/AOBDataset/AOB_TF/",
+        "path": "trained_models2/MNV2_ft_06/",
+        "include_bag": True,
+        "color_data": True,
+        "dataset_path": "datasets/AOBDataset/",
         "num_classes": 3,
         "classes": [],
         "input_shape": (224, 224, 3),
         "epochs": 20,
-        "batch_size": 30,
+        "ft_epochs": 20,
+        "batch_size": 50,
         "loss": "categorical_crossentropy",
         "metrics": ["accuracy"],
-        "learning_rate": 3e-4,
+        "learning_rate": 0.0001,
         "seed": 123321,
         "dataset_info": " "
     }
+
+    if setup["color_data"] == True:
+        setup["dataset_path"] += "AOB_BAG_COLOR/"
+    else:
+        if setup["include_bag"] == True:
+            setup["dataset_path"] += "AOB_TF/"
+        else:
+            setup["dataset_path"] += "AOB_TF_NB/"
+
 
     return setup
 
@@ -73,19 +85,22 @@ def save_setup(setup):
 #   setup: dictionary with the training setup of the model
 def dataset_pipeline(setup):
     # loads the dataset from AOB
-    train, test, info = load_dataset(path=setup["dataset_path"])
+    train, test, info = load_dataset(path=setup["dataset_path"],
+            color_data=setup["color_data"])
 
     # adds ifnormation of the dataset to the training setup
     setup["dataset_info"] = info
     setup["classes"] = info["categories"]
     setup["num_classes"] = info["num_classes"]
 
-    # DATASER PIPELINE
-    def _join_inputs(x, c, y):
-        return (x, c), y
+    # Checks if there is color data to extract it
+    if setup["color_data"] == True:
+        # DATASER PIPELINE
+        def _join_inputs(x, c, y):
+            return (x, c), y
 
-    #train = train.map(_join_inputs)
-    #test = test.map(_join_inputs)
+        train = train.map(_join_inputs)
+        test = test.map(_join_inputs)
 
     #train = train.map(color_aug)
 
@@ -94,34 +109,102 @@ def dataset_pipeline(setup):
 
     return train, test
 
-def train_model(setup=None, dataset=None):
-    tf.random.set_seed(setup["seed"])
-    np.random.seed(setup["seed"])
-
-    train, test = dataset
+# Function that creates the multi input model
+def multi_input_model(setup):
+    input_img = tf.keras.Input(shape=setup["input_shape"])
+    input_col = tf.keras.Input(shape=(3,))
 
     base_model = tf.keras.applications.MobileNetV2(include_top=False,
             alpha=1.0, weights="imagenet", input_shape=setup["input_shape"])
-    base_model.trainable = False
+    base_model.trainable = False 
 
-    # Agrega un classficador al final del modelo
+    # Adds classifer head at the end of the model
     global_average_layer = tf.keras.layers.GlobalAveragePooling2D(name="gap")
+    conv_dense = tf.keras.layers.Dense(10, activation="relu", name="conv_dense")
+
+    x = base_model(input_img)
+    x = global_average_layer(x)
+    x = conv_dense(x)
+
+    # Numerical data layers
+    num_dense1 = tf.keras.layers.Dense(50, activation="relu", name="color_dense1")
+    #num_dense2 = tf.keras.layers.Dense(100, activation="relu", name="color_dense2")
+
+    y = num_dense1(input_col)
+    #y = num_dense2(y)
+
+    combined = tf.keras.layers.Concatenate()([x, y])
+
     prediction_layer = tf.keras.layers.Dense(setup["num_classes"], name="dense")
     activation_layer = tf.keras.layers.Activation("softmax", name="activation")
 
-    # Crea un nuevo modelo con el base_model y clasficador
+    z = prediction_layer(combined)
+    z = activation_layer(z)
+
+    # Creates the new model
+    model = tf.keras.Model(inputs=[input_img, input_col], outputs=z)
+
+    # Creates layers dictionaire
+    layers_dict = {
+        "base_cnn": base_model,
+        "global_average": global_average_layer,
+        "conv_dense": conv_dense,
+        "num_dense1": num_dense1,
+        #"num_dense2": num_dense2,
+        "concat": combined,
+        "prediction": prediction_layer,
+        "activation": activation_layer }
+
+    return model, layers_dict
+
+# Function that creates the standard model
+def std_model(setup):
+    base_model = tf.keras.applications.MobileNetV2(include_top=False,
+            alpha=1.0, weights="imagenet", input_shape=setup["input_shape"])
+    base_model.trainable = False 
+
+    # Adds classifer head at the end of the model
+    global_average_layer = tf.keras.layers.GlobalAveragePooling2D(name="gap")
+
+    prediction_layer = tf.keras.layers.Dense(setup["num_classes"], name="dense")
+    activation_layer = tf.keras.layers.Activation("softmax", name="activation")
+
+    # Creates the new model
     model = tf.keras.Sequential([
         base_model,
         global_average_layer,
         prediction_layer,
         activation_layer])
 
+    # Creates layer dictionaire
+    layers_dict = {
+        "base_cnn": base_model,
+        "global_average": golbal_average_layer,
+        "prediction": prediction_layer,
+        "activation": activation_layer }
+
+    return model, layers_dict
+
+# Function that trains the model
+def train_model(setup=None, dataset=None):
+    tf.random.set_seed(setup["seed"])
+    np.random.seed(setup["seed"])
+
+    train, test = dataset
+
+    if setup["color_data"] == True:
+        model, l_dict = multi_input_model(setup)
+    else:
+        model, l_dict = std_model(setup)
+
+    base_model = l_dict["base_cnn"]
+
     # Plots the model
     plot_model(model, to_file=setup["path"]+"model_architecture.png",
         show_shapes=True, show_layer_names=True, expand_nested=False)
 
     # Compiles the model
-    opt = Adam(lr=setup["learning_rate"])
+    opt = RMSprop(lr=setup["learning_rate"])
     model.compile(loss=setup["loss"], optimizer=opt, metrics=setup["metrics"])
 
     # Model callbacks
@@ -137,12 +220,13 @@ def train_model(setup=None, dataset=None):
 
     # Fine tunning the mode
     base_model.trainable = True
+    # Num of layers in the base model: 155
     fine_tune_at = 100
 
     for layer in base_model.layers[:fine_tune_at]:
         layer.trainable = False
 
-    opt = Adam(lr=setup["learning_rate"]/10)
+    opt = RMSprop(lr=setup["learning_rate"]/10)
     model.compile(loss=setup["loss"], optimizer=opt, metrics=setup["metrics"])
 
     # Model callbacks
@@ -152,9 +236,18 @@ def train_model(setup=None, dataset=None):
     ]
 
     # Trains the model
-    fine_tune_epochs = 10
-    total_epochs = setup["epochs"] + fine_tune_epochs
+    print("[INFO] Fine tune phase")
+    total_epochs = setup["epochs"] + setup["ft_epochs"]
     _ = model.fit(train, initial_epoch=setup["epochs"], epochs=total_epochs,  
             callbacks=callbacks, validation_data=test)
+
+    # Saves model
+    print("[INFO] Serializing model")
+    model.save(setup["path"] + "model.h5")
+
+    # Graph model metrics
+    print("[INFO] Graphing metrics")
+    graph_model_metrics(csv_path=setup["path"] + "training_log.csv",
+            img_path=setup["path"]+"metrics_graph.png")
 
 main()
